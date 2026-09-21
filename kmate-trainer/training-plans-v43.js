09952cf233d29558e8f6c8825287e53502e19316
karankohli14-creator <karankohli14@gmail.com>
2026-09-21 13:28:21 -0700

    Add K-Mate v43 Training Plan loader

---
+++ kmate-trainer/training-plans-v43.js
@@ -0,0 +1,13 @@
+const TP_LOADER_VERSION = '43.0.0';
+const partUrls = [1, 2, 3, 4, 5, 6, 7].map((number) => `./training-plans-v43-part${number}.txt?v=${TP_LOADER_VERSION}`);
+const responses = await Promise.all(partUrls.map(async (url) => {
+  const response = await fetch(url, { cache: 'no-store' });
+  if (!response.ok) throw new Error(`Unable to load ${url}: ${response.status}`);
+  return response.text();
+}));
+const moduleUrl = URL.createObjectURL(new Blob([responses.join('')], { type: 'text/javascript' }));
+try {
+  await import(moduleUrl);
+} finally {
+  URL.revokeObjectURL(moduleUrl);
+}
